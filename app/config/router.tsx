0cc34b02711f802c4946dd86cb05f620c9024f6a
szymon <szymon_niewczas@wp.pl>
2019-04-27 10:57:12 +0200

    Remove tab navigator

--- app/config/router.tsx
+++ app/config/router.tsx
@@ -1,33 +1,14 @@
-import { createBottomTabNavigator, createAppContainer, createStackNavigator } from 'react-navigation';
-import React from "react"
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 import { MemoListScreen } from "../screens/MemoListScreen"
-import secondScreen from "../screens/SecondScreen"
 import { MemoSeriesPreviewScreen } from "../screens/MemoSeriesPreview"
-import Icon from "react-native-vector-icons/Ionicons"
 import { MemoSeriesDetails } from "../screens/MemoSeriesDetails"
-const tabNavigator = createBottomTabNavigator({
-    First: {
-        screen: MemoListScreen,
-        navigationOptions: {
-            title: "Memos",
-            tabBarLabel: "Your Memos",
-            tabBarIcon: () => <Icon name={"md-list-box"} size={30}></Icon>
-        },
-
-    },
-    Second: secondScreen,
-
-}, {
-        tabBarOptions: {
-            labelStyle: {
-                fontSize: 16,
-            }
-        }
-    })
 
 const routeNavigator = createStackNavigator({
     Default: {
-        screen: tabNavigator,
+        screen: MemoListScreen,
+        navigationOptions: {
+            title: "Your Memos"
+        }
     },
     MemoSeries: {
         screen: MemoSeriesPreviewScreen,
