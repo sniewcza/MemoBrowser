6db5d23f64c45c3efd9b92b133101b0ddee9530c
jenkins <jenkins@Mac-mini-jenkins.local>
2020-04-14 23:13:38 +0200

    RN version upgrade 59.5 -> 61.1 IOS part

--- app/components/MemoList/MemoSwipeRow.tsx
+++ app/components/MemoList/MemoSwipeRow.tsx
@@ -1,162 +1,129 @@
 import React, { FC, useEffect, useState } from "react";
-import { SwipeRow } from "native-base"
-import { View, StyleSheet, TouchableNativeFeedback, TextInput, Keyboard } from "react-native"
-import SubMenuButton from "../Buttons/MemoListItemSubmenuButton"
-import Icon from "react-native-vector-icons/Ionicons"
-import { deleteMemoAlert } from "../Alerts/DeleteMemoAlert"
-import { MemoListItem } from "./MemoListItem"
+import {
+  View,
+  StyleSheet,
+  TouchableNativeFeedback,
+  TextInput,
+  Keyboard,
+} from "react-native";
+import SubMenuButton from "../Buttons/MemoListItemSubmenuButton";
+import Icon from "react-native-vector-icons/Ionicons";
+import { deleteMemoAlert } from "../Alerts/DeleteMemoAlert";
+import { MemoListItem } from "./MemoListItem";
 import { Memo } from "../../model";
 import { Color } from "../../config/ColorTheme";
 
 interface Props {
-    memo: Memo
-    deletionMode: boolean
-    onDelete: (id: string) => any;
-    onPress: (id: string) => any
-    onLongPress: () => any
-    onRename: (id: string, newName: string) => any
-    onCheck: (id: string) => any
+  memo: Memo;
+  deletionMode: boolean;
+  onDelete: (id: string) => any;
+  onPress: (id: string) => any;
+  onLongPress: () => any;
+  onRename: (id: string, newName: string) => any;
+  onCheck: (id: string) => any;
 }
 
-export const MemoSwipeRow: FC<Props> = props => {
-    let row: SwipeRow | null
-    const [renameMode, setRenameMode] = useState<boolean>(false)
-    const [checked, setChecked] = useState(false)
-    const [memoName, setMemoName] = useState("")
+export const MemoSwipeRow: FC<Props> = (props) => {
+  let row: SwipeRow | null;
+  const [renameMode, setRenameMode] = useState<boolean>(false);
+  const [checked, setChecked] = useState(false);
+  const [memoName, setMemoName] = useState("");
 
-    useEffect(() => {
-        return () => Keyboard.addListener("keyboardDidHide", keyboardDidHide).remove()
-    }, [])
+  useEffect(() => {
+    return () =>
+      Keyboard.addListener("keyboardDidHide", keyboardDidHide).remove();
+  }, []);
 
-    useEffect(() => {
-        closeRow()
-        setChecked(false)
-    }, [props.deletionMode])
+  useEffect(() => {
+    closeRow();
+    setChecked(false);
+  }, [props.deletionMode]);
 
-    const keyboardDidHide = () => {
-        if (renameMode) {
-            setRenameMode(false)
-        }
+  const keyboardDidHide = () => {
+    if (renameMode) {
+      setRenameMode(false);
     }
+  };
 
-    const deleteMemo = () => {
-        closeRow()
-        deleteMemoAlert(() => {
-            props.onDelete(props.memo.id)
-        })
+  const deleteMemo = () => {
+    closeRow();
+    deleteMemoAlert(() => {
+      props.onDelete(props.memo.id);
+    });
+  };
+
+  const onPress = () => {
+    if (props.deletionMode) {
+      setChecked(!checked);
+      props.onCheck(props.memo.id);
+    } else {
+      closeRow();
+      props.onPress(props.memo.id);
     }
+  };
 
-    const onPress = () => {
-        if (props.deletionMode) {
-            setChecked(!checked)
-            props.onCheck(props.memo.id)
-        }
-        else {
-            closeRow()
-            props.onPress(props.memo.id)
-        }
-    }
+  const closeRow = () => {
+    if (row) row._root.closeRow();
+  };
 
-    const closeRow = () => {
-        if (row)
-            row._root.closeRow();
-    }
+  const renameMemo = () => {
+    closeRow();
+    setRenameMode(true);
+  };
 
-    const renameMemo = () => {
-        closeRow()
-        setRenameMode(true)
-    }
+  const closeTextInput = () => {
+    props.onRename(props.memo.id, memoName);
+    setRenameMode(false);
+  };
 
-    const closeTextInput = () => {
-        props.onRename(props.memo.id, memoName)
-        setRenameMode(false)
-    }
+  const handleTextChange = (text: string) => {
+    setMemoName(text);
+  };
 
-    const handleTextChange = (text: string) => {
-        setMemoName(text)
-    }
-
-    const { deletionMode, onLongPress, memo } = props
-    return (
-        <SwipeRow
-            style={styles.swipeRow}
-            rightOpenValue={-100}
-            stopRightSwipe={-100}
-            disableRightSwipe
-            disableLeftSwipe={deletionMode}
-            ref={ref => row = ref}
-            body={!renameMode ?
-                <TouchableNativeFeedback
-                    onPress={onPress}
-                    delayLongPress={100}
-                    onLongPress={onLongPress}>
-                    <MemoListItem
-                        style={styles.listItem}
-                        name={memo.name}
-                        creationDate={memo.creationDate}
-                        photosCount={memo.photos.length}
-                        deletionMode={deletionMode}
-                        checked={checked}
-                        thumbnail={memo.photos[0].uri}
-                    />
-                </TouchableNativeFeedback>
-                :
-                <View style={styles.listItem}>
-                    <TextInput
-                        autoFocus
-                        placeholder={memoName}
-                        value={memoName}
-                        onChangeText={handleTextChange}
-                        onBlur={closeTextInput}
-                    />
-                </View>
-            }
-            right={
-                <View style={styles.subMenu}>
-                    <SubMenuButton onPress={renameMemo} style={styles.renameButton}>
-                        <Icon name="md-create" size={30} color={Color.onPrimary} />
-                    </SubMenuButton>
-                    <SubMenuButton onPress={deleteMemo} style={styles.deleteButton}>
-                        <Icon name="md-trash" size={30} color={Color.onPrimary} />
-                    </SubMenuButton>
-                </View>
-            }
-        />
-
-    )
-
-}
+  const { deletionMode, onLongPress, memo } = props;
+  return (
+    <MemoListItem
+      style={styles.listItem}
+      name={memo.name}
+      creationDate={memo.creationDate}
+      photosCount={memo.photos.length}
+      deletionMode={deletionMode}
+      checked={checked}
+      thumbnail={memo.photos[0].uri}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
-    swipeRow: {
-        width: "100%",
-        height: 56,
-        paddingRight: 0, // override default swpieRow styles
-        paddingTop: 0,// override default swpieRow styles
-        paddingBottom: 0,// override default swpieRow styles
-        borderBottomColor: 'black',
-        borderBottomWidth: StyleSheet.hairlineWidth
-    },
-    listItem: {
-        flexDirection: 'row',
-        width: "100%",
-        height: "100%",
-        paddingRight: 5,
-        alignItems: "center",
-    },
-    subMenu: {
-        marginVertical: 1,
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "stretch",
-    },
-    deleteButton: {
-        flex: 1,
-        backgroundColor: Color.primary,
-    },
-    renameButton: {
-        flex: 1,
-        backgroundColor: Color.primary,
-    }
-})
+  swipeRow: {
+    width: "100%",
+    height: 56,
+    paddingRight: 0, // override default swpieRow styles
+    paddingTop: 0, // override default swpieRow styles
+    paddingBottom: 0, // override default swpieRow styles
+    borderBottomColor: "black",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  listItem: {
+    flexDirection: "row",
+    width: "100%",
+    height: "100%",
+    paddingRight: 5,
+    alignItems: "center",
+  },
+  subMenu: {
+    marginVertical: 1,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "stretch",
+  },
+  deleteButton: {
+    flex: 1,
+    backgroundColor: Color.primary,
+  },
+  renameButton: {
+    flex: 1,
+    backgroundColor: Color.primary,
+  },
+});
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-/**
- * @format
- */
-import { Provider } from "react-redux"
-import React from "react"
-import { AppRegistry } from 'react-native';
-import App from './app/config/Router';
-import { store } from "./app/store/configureStore"
-import { name as appName } from './app.json';
+import { Provider } from "react-redux";
+import React from "react";
+import { AppRegistry } from "react-native";
+import App from "./app/config/Router";
+import { store } from "./app/store/configureStore";
+import { name as appName } from "./app.json";
+import Icon from "react-native-vector-icons/Ionicons";
+Icon.loadFont();
 
 const connectedApp = () => (
-    <Provider store={store}>
-        <App></App>
-    </Provider>
-)
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 AppRegistry.registerComponent(appName, () => connectedApp);
